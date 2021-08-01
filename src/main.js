ffd062f3d78710f020b5a364e49dd2e456cfaddf
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2021-08-01 12:35:37 -0700

    Добавление компонента меню

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import {createUserRateTemplate} from './view/user-rate.js';
-
+//Отрисовка компонента Звание пользователя
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteMainElement = document.querySelector('.header');
-const siteHeaderElement = siteMainElement.querySelector('.header__logo');
+const siteHeader = document.querySelector('.header');
+const siteHeaderElement = siteHeader.querySelector('.header__logo');
 
 render(siteHeaderElement, createUserRateTemplate(), 'afterend');
+
+//Отрисовка компонента фильтров и статистики (меню)
+import {createMenuFilterTemplate} from './view/menu-stats.js';
+const siteMenuFilter = document.querySelector('.main');
+
+render (siteMenuFilter, createMenuFilterTemplate(), 'afterbegin');
